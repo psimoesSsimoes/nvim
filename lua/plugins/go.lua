e7dd8dfb196f560c6a406b6d057bf10fec83c291
Pedro Simões <pedro.simoes@easypay.pt>
2025-02-07 11:42:30 +0000

    [NI] removing go definition

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -7,7 +7,7 @@ return {
       -- Go settings
       vim.g.go_fmt_command = "goimports"
       vim.g.go_auto_type_info = 1
-      vim.g.go_def_mode = 'gopls'
+      -- vim.g.go_def_mode = 'gopls'
       vim.g.go_info_mode = 'gopls'
       
       -- Highlighting
